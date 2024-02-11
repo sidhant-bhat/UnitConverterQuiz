95535e028eb5ab64287e65629d821588f6d8e535
sidhant-bhat <bhat.sid2001@gmail.com>
2024-02-11 11:37:35 +0530

    no changes added

---
+++ app/src/main/java/com/example/unitconverterquiz/CounterModel.kt
@@ -0,0 +1,15 @@
+package com.example.unitconverterquiz
+
+data class CounterModel(var count : Int)
+
+class CounterRepository{
+    private var _counter = CounterModel(0)
+    fun getCounter()= _counter
+
+    fun incrementCounter(){
+        _counter.count++
+    }
+    fun decrementCounter(){
+        _counter.count--
+    }
+}
